5e7ce9b4f6a9d26c584d83b9c0a8176a84c2b2af
Isaiah Peng <issaria@gmail.com>
2013-04-02 00:41:34 +0200

    update doc.go fix insert return id for postgres

--- doc.go
+++ doc.go
@@ -2,7 +2,7 @@ package main
 
 import (
         "fmt"
-        _ "github.com/bmizerany/pq"
+        _ "github.com/lib/pq"
         "database/sql"
         //"github.com/garyburd/gopkgdoc/doc"
         "github.com/garyburd/gopkgdoc/database"
@@ -10,7 +10,7 @@ import (
         "bytes"
         godoc "go/doc"
         //"net/http"
-        //"time"
+        "time"
 )
 
 func main() {
@@ -43,44 +43,50 @@ func main() {
         if err != nil {
                 panic(err)
         }
-        fmt.Println(pkg.ImportPath)
-        fmt.Println(pkg.Doc)
-        var nsSql = "insert into namespaces (name, doc, version, library_id) values ($1, $2, '1.0.3', 1)"
-        //var funSql = "insert into functions (name, doc, arglists_comp, version, url_friendly_name, namespace_id) values ($1, $2, $3, '1.0.3', $4, 4)"
-        _, err = pg.Exec(nsSql, pkg.ImportPath, comment(pkg.Doc))
-        if err != nil {
-                panic(err)
+        //var nsSql = pg.Prepare("insert into namespaces (name, doc, version, library_id) values ($1, $2, '1.0.3', 1)")
+        funSql, err := pg.Prepare("insert into functions (name, doc, arglists_comp, version, url_friendly_name, functional_id, functional_type) values ($1, $2, $3, '1.0.3', $4, $5, $6)")
+        check(err)
+        typeSql := "insert into type_classes (name, doc, arglists_comp, type, namespace_id, version, created_at, updated_at) values ($1, $2, $3, 'StructType', 25, '1.0.3', $4, $5) RETURNING id"
+        check(err)
+        //_, err = pg.Exec(nsSql, pkg.ImportPath, comment(pkg.Doc))
+        //if err != nil {
+        //        panic(err)
+        //}
+        now := time.Now()
+
+        for _, fun := range pkg.Funcs {
+                _, err = funSql.Exec(fun.Name, comment(fun.Doc), fun.Decl.Text, fun.Name, 25, "Namespace")
+                if err != nil {
+                        panic(err)
+                }
         }
 
-        //for _, fun := range pkg.Funcs {
-        //        _, err = pg.Exec(funSql, fun.Name, fun.Doc, fun.Decl.Text, comment(fun.Name))
-        //        if err != nil {
-        //                panic(err)
-        //        }
-        //}
+        for _, t := range pkg.Types {
+                var id int
+                err = pg.QueryRow(typeSql, t.Name, comment(t.Doc), t.Decl.Text, now, now).Scan(&id)
+                check(err)
+                fmt.Println("TypeClass ID", id)
+                for _, fun := range t.Funcs {
+                        _, err = funSql.Exec(fun.Name, comment(fun.Doc), fun.Decl.Text, fun.Name, id, "TypeClass")
+                        if err != nil {
+                                panic(err)
+                        }
+                }
 
-        //for _, t := range pkg.Types {
-        //        fmt.Println(t.Name)
-        //        fmt.Println(t.Decl.Text)
-        //        fmt.Println(t.Doc)
-        //        for _, fun := range t.Funcs {
-        //                fmt.Println(fun.Name)
-        //                fmt.Println(fun.Decl.Text)
-        //                fmt.Println(fun.Doc)
-        //        }
-        //        for _, fun := range t.Methods {
-        //                fmt.Println(fun.Name)
-        //                fmt.Println(fun.Decl.Text)
-        //                fmt.Println(fun.Doc)
-        //        }
+                for _, fun := range t.Methods {
+                        _, err = funSql.Exec(fun.Name, comment(fun.Doc), fun.Decl.Text, fun.Name, id, "TypeClass")
+                        if err != nil {
+                                panic(err)
+                        }
+                }
 
-        //        for _, eg := range t.Examples {
-        //                fmt.Println(eg.Name)
-        //                fmt.Println(eg.Code.Text)
-        //                fmt.Println(eg.Doc)
-        //                fmt.Println(eg.Output)
-        //        }
-        //}
+                //for _, eg := range t.Examples {
+                //        fmt.Println(eg.Name)
+                //        fmt.Println(eg.Code.Text)
+                //        fmt.Println(eg.Doc)
+                //        fmt.Println(eg.Output)
+                //}
+        }
 }
 
 var (
@@ -107,5 +113,11 @@ func comment(v string) string {
         p = rfcRE.ReplaceAll(p, rfcReplace)
         // rollback the links in code
         //p = linkInCode.ReplaceAll(p, linkInCodeReplace)
+        //fmt.Println(string(p))
         return string(p)
+}
+func check(err error) {
+        if err != nil {
+                panic(err)
+        }
 }
