f0a243ea8e4765ce4ceea950352342d3faa6b345
isaiah <issaria@gmail.com>
2013-06-02 13:04:11 +0200

    login without condition

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,7 @@
 class SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate!(auth_options)
-    if resource.email.blank? || resource.login.blank?
-      redirect_to new_user_path(identity_url: resource.identity_url)
-    else
-      sign_in(:user, resource, bypass: true)
-      respond_with resource, location: after_sign_in_path_for(resource)
-      #set_flash_message(:notice, :signed_in) if is_navigational_format?
-      #sign_in(:user, resource)
-      #redirect_to 
-    end
+    sign_in(:user, resource, bypass: true)
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 end
